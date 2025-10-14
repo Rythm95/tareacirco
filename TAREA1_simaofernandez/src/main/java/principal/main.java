842e5a5501ca6493ac197f71d40f8c5bfb6f35a8
Simao <Bicho1000s@gmail.com>
2025-10-14 16:36:10 +0200

    test

--- TAREA1_simaofernandez/src/main/java/principal/main.java
+++ TAREA1_simaofernandez/src/main/java/principal/main.java
@@ -10,7 +10,8 @@ public class main {
 		LocalDate date = LocalDate.of(2020, 1, 8);
 		
 		new Espectaculo(1L, "Malabarismos", LocalDate.of(2023, 12, 10),LocalDate.of(2024, 1, 6));
-		new Espectaculo(2L, "Malabarismos", LocalDate.of(2024, 1, 8),LocalDate.of(2024, 2, 8));
+		new Espectaculo(2L, "Trapecismo", LocalDate.of(2024, 1, 8),LocalDate.of(2024, 2, 8));
+		new Espectaculo(3L, "Monólogos", LocalDate.of(2024, 2, 8),LocalDate.of(2024, 2, 8));
 		
 	}
 
