d3597e27e6a7108e42379905c01e844d5a96a8ac
Simao <Bicho1000s@gmail.com>
2025-10-14 16:47:04 +0200

    test pull

--- TAREA1_simaofernandez/src/main/java/principal/main.java
+++ TAREA1_simaofernandez/src/main/java/principal/main.java
@@ -12,7 +12,7 @@ public class main {
 		new Espectaculo(1L, "Malabarismos", LocalDate.of(2023, 12, 10),LocalDate.of(2024, 1, 6));
 		new Espectaculo(2L, "Trapecismo", LocalDate.of(2024, 1, 8),LocalDate.of(2024, 2, 8));
 		new Espectaculo(3L, "Monólogos", LocalDate.of(2024, 2, 8),LocalDate.of(2024, 2, 8));
-		
+		// test
 	}
 
 }
